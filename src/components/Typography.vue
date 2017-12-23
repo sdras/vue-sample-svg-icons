97a29d8eb7ceaf2a675ad8d30dfaafaf23c6cc65
sdras <sarah.drasner@gmail.com>
2017-12-23 13:56:49 -0700

    put in comments

--- src/components/Typography.vue
+++ src/components/Typography.vue
@@ -2,15 +2,21 @@
   <div>
     <h1>SVG Icon System</h1>
     <p>
+      <!--you can pass in a smaller width and height as props-->
+      <icon-base width="12" height="12"><icon-write /></icon-base> 
+      <!--or you can use the default, which is 18-->
       <icon-base><icon-write /></icon-base>
+      <!--or make it a little bigger too :) -->
       <icon-base width="30" height="30"><icon-write /></icon-base> 
       These are inline icons
     </p>
     <p style="color:red">
+      <!--you can change the color of the text, and the icons will automatically change color as well-->
       <icon-base><icon-moon /></icon-base> 
       These are red inline icons
     </p>
     <p>
+      <!--because the base is consistent, you can use multiple icons on the same line and they're all in order-->
       <icon-base><icon-calendar /></icon-base>
       <icon-base><icon-image /></icon-base>
       <icon-base><icon-write /></icon-base>
