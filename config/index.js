46a5e03db1581a6df048d147c136050baaec4e3a
sdras <sarah.drasner@gmail.com>
2017-12-24 10:17:34 -0700

    redeploy docs

--- config/index.js
+++ config/index.js
@@ -41,12 +41,12 @@ module.exports = {
 
   build: {
     // Template for index.html
-    index: path.resolve(__dirname, '../docs/index.html'),
+    index: path.resolve(__dirname, '../dist/index.html'),
 
     // Paths
-    assetsRoot: path.resolve(__dirname, '../docs'),
+    assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '',
 
     /**
      * Source Maps
